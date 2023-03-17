f85d48b214c1b874f40394fb2c81ddb2813937f0
Austin <gilmoursa@gmail.com>
2023-03-17 17:41:45 -0400

    weather app first commit
    I got a bit carried away. I should have committed more along the way. Anyway, here's a weather app that uses the Open Weather API. I used this project as a way to learn about pulling data from APIs.

---
+++ app.js
@@ -0,0 +1,48 @@
+const apiKey = 'f9273d20c395f8af5fa67d733595fbb6';
+
+const form = document.querySelector('form');
+const input = document.querySelector('#city');
+const weatherDiv = document.querySelector('#weather');
+const body = document.querySelector('body');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const city = input.value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const { main, name, weather } = data;
+            const description = weather[0].description;
+            const temp = main.temp.toFixed(1);
+            const feelsLike = main.feels_like.toFixed(1);
+            const humidity = main.humidity;
+            const windSpeed = data.wind.speed.toFixed(0);
+            const sunrise = data.sys.sunrise;
+            const sunset = data.sys.sunset;
+            if (temp < 70) {
+                body.classList.remove("hot");
+                body.classList.add("cold");
+            } else {
+                body.classList.add("hot");
+                body.classList.remove("cold");
+            }
+            weatherDiv.innerHTML = `
+        <p>Current weather in </p><h2>${name}</h2> 
+        <img src="http://openweathermap.org/img/wn/${weather[0].icon}.png" alt="${description}">
+        <p>${description}</p>
+        <h2>Temperature:</h2><p> ${temp}<sup>&deg;F</sup></p>
+        <h2>Feels Like:</h2><p> ${feelsLike}<sup>&deg;F</sup></p>
+        <h2>Humidity:</h2><p> ${humidity}%</p>
+        <h2>Wind Speed:</h2><p> ${windSpeed} mph</p>
+        <h2>Sunrise:</h2><p> ${new Date(sunrise * 1000).toLocaleTimeString()}</p>
+        <h2>Sunset:</h2><p> ${new Date(sunset * 1000).toLocaleTimeString()}</p>
+      `;
+
+        })
+        .catch(error => {
+            console.error(error);
+            weatherDiv.innerHTML = `<p>Something went wrong! Please try again.</p>`;
+        });
+});
