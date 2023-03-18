b7294b8de1383b087640d946f103f57e00b4ad9c
Austin <gilmoursa@gmail.com>
2023-03-18 10:55:14 -0400

    removed the feelsLike parameter, added option to convert F to C and back, autolocation added instead of city field

--- app.js
+++ app.js
@@ -1,59 +1,85 @@
 const apiKey = 'f9273d20c395f8af5fa67d733595fbb6';
-
 const form = document.querySelector('form');
 const input = document.querySelector('#city');
 const weatherDiv = document.querySelector('#weather');
 const mapDiv = document.querySelector('#map');
 const body = document.querySelector('body');
+const temperatureSection = document.querySelector('.temperature');
+const temperatureSpan = document.querySelector('.temperature span');
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const city = input.value;
-    const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+const nameClass = document.querySelector('.name');
+const descriptionClass = document.querySelector('.description');
+const tempClass = document.querySelector('.temperature-degree');
+const humidityClass = document.querySelector('.humidity');
+const windSpeedClass = document.querySelector('.wind-speed');
+const sunriseClass = document.querySelector('.sunrise');
+const sunsetClass = document.querySelector('.sunset');
+const weatherImageClass = document.querySelector('.weather-img');
 
-    // get weather data from API
-    fetch(weatherUrl)
-        .then(response => response.json())
-        .then(data => {
-            const { main, name, weather } = data;
-            const description = weather[0].description;
-            const temp = main.temp.toFixed(1);
-            const feelsLike = main.feels_like.toFixed(1);
-            const humidity = main.humidity;
-            const windSpeed = data.wind.speed.toFixed(0);
-            const sunrise = data.sys.sunrise;
-            const sunset = data.sys.sunset;
-            if (temp < 70) {
-                body.classList.remove("hot");
-                body.classList.add("cold");
-            } else {
-                body.classList.add("hot");
-                body.classList.remove("cold");
-            }
-            weatherDiv.innerHTML = `
-            <div class="weather-container">
-                <div class="weather-col-one">
-                    <p>Current weather in </p>
-                    <h2>${name}</h2> 
-                    <img src="http://openweathermap.org/img/wn/${weather[0].icon}.png" alt="${description}">
-                    <p>${description}</p>
-                    <h2>Temperature:</h2>
-                    <p> ${temp}<sup>&deg;F</sup></p>
-                    <h2>Feels Like:</h2>
-                    <p> ${feelsLike}<sup>&deg;F</sup></p>
-                </div>
-                <div class="weather-col-two">
-                    <h2>Humidity:</h2><p> ${humidity}%</p>
-                    <h2>Wind Speed:</h2><p> ${windSpeed} mph</p>
-                    <h2>Sunrise:</h2><p> ${new Date(sunrise * 1000).toLocaleTimeString()}</p>
-                    <h2>Sunset:</h2><p> ${new Date(sunset * 1000).toLocaleTimeString()}</p>
-                </div>
-            </div>
-      `;
-        })
-        .catch(error => {
-            console.error(error);
-            weatherDiv.innerHTML = `<p>Something went wrong! Please try again.</p>`;
+
+window.addEventListener('load', () => {
+    // get user's location
+    let long;
+    let lat;
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+            const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=imperial`;
+
+            // get weather data from API
+            fetch(weatherUrl)
+                .then(response => response.json())
+                .then(data => {
+                    const { main, name, weather } = data;
+                    const description = weather[0].description;
+                    const icon = weather[0].icon;
+                    const tempF = main.temp.toFixed(1);
+                    const humidity = main.humidity;
+                    const windSpeed = data.wind.speed.toFixed(0);
+                    const sunrise = data.sys.sunrise;
+                    const sunset = data.sys.sunset;
+                    const tempC = ((tempF - 32) * (5 / 9)).toFixed(1);
+                    // change background color based on temperature
+                    if (tempF < 70) {
+                        body.classList.remove("hot");
+                        body.classList.add("cold");
+                    } else {
+                        body.classList.add("hot");
+                        body.classList.remove("cold");
+                    }
+
+                    // format and display weather data
+                    nameClass.textContent = name;
+                    weatherImageClass.innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}.png" alt="${description}">`;
+                    descriptionClass.textContent = description;
+                    tempClass.textContent = tempF;
+                    humidityClass.textContent = humidity;
+                    windSpeedClass.textContent = windSpeed;
+                    sunriseClass.textContent = new Date(sunrise * 1000).toLocaleTimeString();
+                    sunsetClass.textContent = new Date(sunset * 1000).toLocaleTimeString();
+
+                    // change temperature to Celsius/Fahrenheit
+                    temperatureSection.addEventListener('click', () => {
+                        if (temperatureSpan.textContent === "F") {
+                            temperatureSpan.textContent = "C";
+                            tempClass.textContent = tempC;
+                        }else {
+                            temperatureSpan.textContent = "F";
+                            tempClass.textContent = tempF;
+                        }
+                    });
+                    
+
+                })
+                // display error message if API call fails
+                .catch(error => {
+                    console.error(error);
+                    weatherDiv.innerHTML = `<p>Something went wrong! Please try again.</p>`;
+                });
         });
-
+    } else {
+        // display error message if geolocation is not supported
+        console.log('Geolocation is not supported by this browser.');
+    }
 });
